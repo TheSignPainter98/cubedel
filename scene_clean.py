8861f79108437ad17359c44aaaa817141e928990
Ed Jones <edjones98@gmail.com>
2020-08-05 17:55:32 +0100

    Clean away all objects

--- scene_clean.py
+++ scene_clean.py
@@ -2,7 +2,7 @@
 
 from bpy import context, ops
 
-def cubedel():
+def clean_scene():
     ctx:dict = context.copy()
-    ctx['selected_objects'] = [ctx['scene'].objects['Cube']]
+    ctx['selected_objects'] = ctx['scene'].objects
     ops.object.delete(ctx)
